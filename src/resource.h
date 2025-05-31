5bb60340cc8238ba40dc019c7bcd9e607679b3f9
Fr33dan <D4rk_fr33d3n@hotmail.com>
2025-05-31 11:02:18 -0400

    Resource.h is used with the .rc file, no generated by it. Should include in repo.

---
+++ src/resource.h
@@ -0,0 +1,58 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ControllerModeMonitor.rc
+//
+#define IDC_MYICON                      2
+#define IDD_CONTROLLERMODEMONITOR_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDS_CONTROLLER                  104
+#define IDM_EXIT                        105
+#define IDS_ADD_PROMPT1_TITLE           105
+#define IDS_ADD_PROMPT2_TITLE           106
+#define IDI_CONTROLLERMODEMONITOR       107
+#define IDS_ADD_CONFIRMATION            107
+#define IDI_SMALL                       108
+#define IDS_CONFIRM                     108
+#define IDC_CONTROLLERMODEMONITOR       109
+#define IDS_REMOVE_PROMPT               110
+#define IDS_CMD_BIG_PICTURE_ACTIVATE    111
+#define IDS_CMD_BIG_PICTURE_DEACTIVATE  112
+#define IDS_CONFIG_LIST_LOCATION        113
+#define IDS_CONFIG_PATH                 114
+#define IDS_CONFIG_TV                   115
+#define IDS_TV_NOT_FOUND                116
+#define IDS_BALLOON_TITLE               117
+#define IDS_CONFIG_NOT_FOUND            118
+#define IDS_RESTORE_AUDIO_DEVICE        119
+#define IDS_AUDIO_DEVICE_NOT_FOUND      120
+#define IDR_MAINFRAME                   128
+#define IDS_ADD_PROMPT1                 130
+#define IDS_ADD_PROMPT2_YES             132
+#define IDS_ADD_PROMPT2_NO              133
+#define ID_FILE_ADDDEVICE               32771
+#define IDM_ADDDEVICE                   32772
+#define ID_ADDDEVICE_ADDDEVICE          32773
+#define ID_FILE_TV                      32774
+#define ID_TV_SEARCH                    32775
+#define IDM_TV_SEARCH                   32776
+#define ID_TV_HDMI1                     32777
+#define ID_FILE_AUDIO                   32778
+#define ID_AUDIO_CLEAR                  32779
+#define IDM_AUDIO_CLEAR                 32780
+#define ID_FILE_RUNATLOGIN              32781
+#define ID_TV_CLEAR                     32782
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif

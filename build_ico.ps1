03ca7bb0a9daa4cac4a4dc1db9b3ae50fbf58397
Fr33dan <D4rk_fr33d3n@hotmail.com>
2025-05-31 02:06:05 -0400

    Fix icon generation

--- build_ico.ps1
+++ build_ico.ps1
@@ -2,13 +2,14 @@ $inkscape = "C:\Program Files\Inkscape\bin\inkscape.com"
 $resolutionList = 256, 128, 64, 48, 32, 24, 16
 Write-Host "Building ICO"
 New-Item -Name "ico" -Type "Directory"
-$imageFile = ""
+$magickArgs = @()
 foreach($resolution in $resolutionList){
 	$filePath = "ico\$resolution.png"
 	Write-Host "Creating $filePath"
 	&$inkscape --export-background-opacity=0 --export-height=$resolution --export-type=png --export-filename=$filePath "vector_logo.svg"
-	$imageFile += "$filePath "
+	$magickArgs += "$filePath"
 }
-magick convert $imageFile ControllerMonitorMode.ico
-Write-Host "Creating ControllerMonitorMode.ico"
+$magickArgs += "ControllerModeMonitor.ico"
+Start-Process magick -ArgumentList $magickArgs -Wait
+Write-Host "Creating ControllerModeMonitor.ico"
 Remove-Item -Path "ico" -Recurse -Force
